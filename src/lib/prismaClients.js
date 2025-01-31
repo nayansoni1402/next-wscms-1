e9a00411fb48310832db9db1dbb275f458ec2822
Nayan Soni <vikram.rathore@woodenstreet.com>
2025-01-31 17:09:59 +0530

    added

--- src/lib/prismaClients.js
+++ src/lib/prismaClients.js
@@ -1,9 +1,27 @@
-import { PrismaClient as BlogPrismaClient } from "@prisma/generated/blog";
-import { PrismaClient as CmsPrismaClient } from "@prisma/generated/cms";
-import { PrismaClient as CrmPrismaClient } from "@prisma/generated/crm";
+import { PrismaClient as BlogPrismaClient } from "../../generated/blog";
 
 const blogDb = new BlogPrismaClient();
-const cmsDb = new CmsPrismaClient();
-const crmDb = new CrmPrismaClient();
 
-export { blogDb, cmsDb, crmDb };
+const BASE_URL = process.env.NEXT_PUBLIC_S3_BLOG_URL;
+
+blogDb.$use(async (params, next) => {
+      const result = await next(params);
+
+      if (params.model === 'Blog' && (params.action === 'findMany' || params.action === 'findUnique')) {
+            if (Array.isArray(result)) {
+                  return result.map(blog => ({
+                        ...blog,
+                        image: blog.image ? `${BASE_URL}${blog.image}` : null,
+                  }));
+            } else if (result) {
+                  return {
+                        ...result,
+                        image: result.image ? `${BASE_URL}${result.image}` : null,
+                  };
+            }
+      }
+
+      return result;
+});
+
+export { blogDb };
---
+++ src/note.jsx
@@ -0,0 +1,6 @@
+const ROBOT_LABELS = {
+      INDEX_FOLLOW: "Index Follow",
+      NOINDEX_NOFOLLOW: "Noindex Nofollow",
+      INDEX_NOFOLLOW: "Index Nofollow",
+      NOINDEX_FOLLOW: "Noindex Follow",
+};
